58b052ebe41046d8e0b141e73cbd129d0c1e6e15
leo <leogonglin@gmail.com>
2015-11-16 23:33:41 +0800

    add mybatis support

--- src/main/java/com/leo/controller/Example.java
+++ src/main/java/com/leo/controller/Example.java
@@ -2,21 +2,31 @@ package com.leo.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.leo.service.TagService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Date;
+import java.util.Map;
 
-@RestController
+@Controller
 public class Example {
 
     @Resource
     private TagService tagService;
 
-    @RequestMapping("/{id}")
-    String home(@PathVariable("id") Long id) {
-        return JSON.toJSONString(tagService.get(id));
+    // @RequestMapping("/{id}")
+    // String home(@PathVariable("id") Long id) {
+    // return JSON.toJSONString(tagService.get(id));
+    // }
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("time", new Date());
+        model.put("message", "hello message");
+        return "welcome";
     }
 
 }
